d4669d59ac5e6fdefc13b2bf1f9f932aa7e37ee3
mm <nathaphong_p@cmu.ac.th>
2022-01-14 17:24:48 +0700

    finished

--- lab16_4.cpp
+++ lab16_4.cpp
@@ -12,8 +12,56 @@ int main(){
 	
 	for(int i = 0;i < 10;i++){
 	    shuffle(a,b,c,d);
-	    cout << a << " " << b << " " << c << " " << d << "\n";
+	    cout << a << " " << b << " " << c << " " << d << "\n";;
 	}
 	
 	return 0;
+}
+
+void shuffle(int &a,int &b,int &c,int &d){
+	int slot[] = {a, b, c, d};
+	int k = rand()%4;
+	int round = 1;
+	
+	a = slot[k];
+	slot[k] = 0;
+
+	while (round != 0)
+	{
+		k = rand()%4;
+		if (slot[k] == 0)
+		{
+			continue;
+			}
+			else{
+
+			b = slot[k];
+			slot[k] = 0;
+
+			for (int i = 0; i < 4; i++)
+			{
+				if (slot[i] != 0)
+				{
+					c = slot[i];
+					slot[i] = 0;
+					break; 
+				}
+				
+			}
+
+			for (int j = 0; j < 4; j++)
+			{
+				if (slot[j] != 0)
+				{
+					d = slot[j];
+					break;
+				}
+			}
+			
+			round = 0;
+		}
+
+		
+	}
+		
 }
--- lab16_5.cpp
+++ lab16_5.cpp
@@ -3,7 +3,7 @@
 #include <ctime>
 using namespace std;
 
-void shuffle(int &,int &, int &, int &); //Modify input arguments to pointer 
+void shuffle(int *,int *, int *, int *); 
 
 int main(){
 	int a = 50, b = 100, c = 500, d = 1000;
@@ -11,11 +11,56 @@ int main(){
 	srand(time(0));	
 	
 	for(int i = 0;i < 10;i++){
-	    shuffle(a,b,c,d); //Modify input arguments to pointer 
+	    shuffle(&a,&b,&c,&d); 
 	    cout << a << " " << b << " " << c << " " << d << "\n";
 	}
 	
 	return 0;
 }
 
-//Write definition of shuffle() using pointer here 
+void shuffle(int *a,int *b,int *c,int *d){
+	int slot[] = {*a, *b, *c, *d};
+	int k = rand()%4;
+	int round = 1;
+	
+	*a = slot[k];
+	slot[k] = 0;
+
+	while (round != 0)
+	{
+		k = rand()%4;
+		if (slot[k] == 0)
+		{
+			continue;
+			}
+			else{
+
+			*b = slot[k];
+			slot[k] = 0;
+
+			for (int i = 0; i < 4; i++)
+			{
+				if (slot[i] != 0)
+				{
+					*c = slot[i];
+					slot[i] = 0;
+					break; 
+				}
+				
+			}
+
+			for (int j = 0; j < 4; j++)
+			{
+				if (slot[j] != 0)
+				{
+					*d = slot[j];
+					break;
+				}
+			}
+			
+			round = 0;
+		}
+
+		
+	}
+}
